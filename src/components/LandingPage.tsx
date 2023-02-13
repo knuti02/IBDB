62ac4508db1c228e54beb8013073b2ac2086be5c
Erik David Thiis-Evensen <ethiisevensen@gmail.com>
2023-02-13 11:38:31 +0100

    [#3]feat: created landing page component co-authored-by: lgdalhau <lgdalhau@stud.ntnu.no>

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPageBookList from './LandingPageBookList'
+import LandingPageHeader from './LandingPageHeader'
+
+export default function LandingPage() {
+  return (
+    <div>
+        <LandingPageHeader />
+        <LandingPageBookList />
+    </div>
+  )
+}
