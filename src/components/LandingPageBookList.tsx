f5967956a9ad62d3b1cc5f38776e75fbc436d3a7
Erik David Thiis-Evensen <ethiisevensen@gmail.com>
2023-02-13 11:34:39 +0100

    [#3]feat: created book list component co-authored-by: lgdalhau <lgdalhau@stud.ntnu.no>

--- src/components/LandingPageBookList.tsx
+++ src/components/LandingPageBookList.tsx
@@ -1,15 +1,42 @@
+import { Stack } from '@mui/material'
 import React from 'react'
+import BookPreview from './BookPreview'
 
 export default function LandingPageBookList() {
 
 
   return (
-    {books.forEach((book) => {
-        return (<BookPreview 
-            title={book.title} 
-            imageSource={book.imageSource} 
-            author={book.author}>
-        )
-    }))}
+    <BookPreview
+        title="Peppa pig"
+        imageSource ="https://images.booksense.com/images/052/310/9780593310052.jpg" 
+        author='Lukas'
+    />
+    // <Stack 
+    //     direction="row"
+    //     flexWrap="wrap"
+    // >
+    //     <BookPreview
+    //         title="Peppa pig"
+    //         imageSource ="https://images.booksense.com/images/052/310/9780593310052.jpg" 
+    //         author='Lukas'
+    //     />
+    //     <BookPreview
+    //         title="Peppa pig"
+    //         imageSource ="https://images.booksense.com/images/052/310/9780593310052.jpg" 
+    //         author='Lukas'
+    //     />
+    //     <BookPreview
+    //     title="Peppa pig"
+    //     imageSource ="https://images.booksense.com/images/052/310/9780593310052.jpg" 
+    //     author='Lukas'
+    //     />
+    // </Stack>
+    // {books.forEach((book) => {
+    //     return (<BookPreview 
+    //         title={book.title} 
+    //         imageSource={book.imageSource} 
+    //         author={book.author}>
+    //     )
+    // }))}
   )
 }
