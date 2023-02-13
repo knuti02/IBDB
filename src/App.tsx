4a5a17c3be9a93bfd3fe0043d2a401898f83fc51
Amalie Mansåker <amalieem@stud.ntnu.no>
2023-02-13 10:46:21 +0100

    [#6] cleanup: reset app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [results, setResults] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`https://openlibrary.org/search.json?q=${searchTerm}`);
-      const data = await response.json();
-      setResults(data.docs);
-    };
-    
-    fetchData();
-  }, [searchTerm]);
-  
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search for a book"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
-      <ul>
-        {results.map(result => (
-          <li key={result.key}>{result.title_suggest}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+function App() {
+  return <div>Hei!</div>;
+}
 
 export default App;
