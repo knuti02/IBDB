a329e6bc057f870ab38cb87618ce8faaf2df51f2
Amalie Mansåker <amalieem@stud.ntnu.no>
2023-02-20 11:06:13 +0100

    [#10] feat: finished add book
    Styled gui and fixed error handling.
    
    Co-authored by armandyb <armandyb@stud.ntnu.no>

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -9,10 +9,14 @@ import {
 import { db } from "../firebase";
 import { Author } from "../types/Author";
 import { Book } from "../types/Book";
+import Button from "@mui/material/Button";
+import Stack from '@mui/material/Stack';
+import { TextField } from "@mui/material";
 
 export default function AddBook() {
   const [bookIsbn, setBookIsbn] = useState("");
   const [status, setStatus] = useState("");
+  const [inputValid, setInputValid] = useState(true);
 
   const fetchBookInfo = async (ISBN: string) => {
     setStatus("Henter bokdata...");
@@ -46,53 +50,75 @@ export default function AddBook() {
   };
 
   const onUpload = async () => {
-    const bookdata = await fetchBookInfo(bookIsbn);
-    const works = await fetchWorkData(bookdata.works[0].key);
-    const author = await fetchAuthorData(works.authors[0].author.key);
-    let book: Book = {
-      title: bookdata.title,
-      description: works.description,
-      author: author.name,
-      isbn_13: bookdata.isbn_13[0],
-      isbn_10: bookdata.isbn_10[0],
-      coverURL:
-        "https://covers.openlibrary.org/b/isbn/" +
-        bookdata.isbn_13[0] +
-        "-M.jpg",
-      publishDate: new Date(bookdata.publish_date),
-      series: bookdata?.series ? bookdata.series : null,
-      numberOfPages: bookdata.number_of_pages,
-      subjects: works.subjects,
-    };
-    setStatus("Bok lastes opp...");
-    await submitBook(book);
-    setStatus("Bok lagt til");
+    if (validateInput()) {
+        setInputValid(true);
+        const bookdata = await fetchBookInfo(bookIsbn);
+        const works = await fetchWorkData(bookdata.works[0].key);
+        const author = await fetchAuthorData(works.authors[0].author.key);
+        let book: Book = {
+        title: bookdata.title,
+        description: works.description,
+        author: author.name,
+        isbn_13: bookdata.isbn_13[0],
+        isbn_10: bookdata.isbn_10[0],
+        coverURL:
+            "https://covers.openlibrary.org/b/isbn/" +
+            bookdata.isbn_13[0] +
+            "-M.jpg",
+        publishDate: new Date(bookdata.publish_date),
+        series: bookdata?.series ? bookdata.series : null,
+        numberOfPages: bookdata.number_of_pages,
+        subjects: works.subjects,
+        };
+        setStatus("Bok lastes opp...");
+        await submitBook(book);
+        setStatus("Bok lagt til");
+    } else {
+        setInputValid(false);
+    }
   };
+
+  const validateInput = () => {
+    return bookIsbn.length === 10 || bookIsbn.length === 13;
+  }
 
   const submitBook = async (book: Book): Promise<void> => {
     try {
-      await setDoc(doc(db, "books", book.isbn_13), book).then(() =>
+        await setDoc(doc(db, "books", book.isbn_13), book).then(() =>
         setStatus("Bok lastet opp!")
-      );
-      setBookIsbn("");
+    ); 
+        setBookIsbn("");      
     } catch (error) {
       console.log("Failed to submit to database" + error + "with data" + book);
       setStatus("Bok opplastning feilet");
     }
   };
 
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+        setBookIsbn(e.currentTarget.value);
+    }
+}
+
   return (
-    <div>
-      <input
+    <Stack
+        direction={"row"}
+        spacing={2}
+        justifyContent="space-between">
+      <TextField
+        label="Skriv inn ISBN"
         type="number"
         data-testid="addBookInputField"
-        placeholder="Skriv inn ISBN"
+        error = { !inputValid }
+        helperText="ISBN må være 10 eller 13 tall"
+        fullWidth
         value={bookIsbn}
         onChange={(e) => setBookIsbn(e.currentTarget.value)}
+        onKeyDown={handleKeyDown}
         //TODO valider antall siffer lagt inn
       />
-      <button onClick={onUpload}>Add book to database</button>
+      <Button size="medium" variant="contained" onClick={onUpload}>Add book to database</Button>
       {status.length > 0 && <p>{status}</p>}
-    </div>
+    </Stack>
   );
 }
