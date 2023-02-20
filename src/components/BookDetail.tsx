80cc122b975ae3c083e65d6cb545535ff890ec9d
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-02-20 08:15:22 +0100

    [#12] feat: added a bookpreview/bookdetail page when a user clicks on a book
    Now the user can se a larger image and text about a given book when clicked

---
+++ src/components/BookDetail.tsx
@@ -0,0 +1,39 @@
+import { Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function BookDetail() {
+  const location = useLocation();
+
+  const { title, author, img_url } = location.state;
+
+  return (
+    <Box boxShadow={4}>
+      <Stack
+        width="100%"
+        height="720px"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Stack direction="row" alignItems="center" justifyContent="content">
+          <img height="500px" src={img_url} alt="ops" />
+          <Stack
+            justifyContent="space-evenly"
+            alignItems="center"
+            width="600px"
+            direction="column"
+            height="500px"
+          >
+            <Stack justifyContent="center">
+              <Typography fontWeight="bold" variant="h3">
+                {title}
+              </Typography>
+              <Typography variant="h4">{author}</Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
