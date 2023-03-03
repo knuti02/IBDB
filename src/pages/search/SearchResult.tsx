1fad5889975fe6cd10b20c3563628cd2df5ff0ba
Amalie Mansåker <amalieem@stud.ntnu.no>
2023-03-03 11:48:47 +0100

    [#13] feat: Styled search result
    Multiple books will now be next to each other
    
    Co-authored by: tirzash <tirzash@stud.ntnu.no>

--- src/pages/search/SearchResult.tsx
+++ src/pages/search/SearchResult.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import BookPreview from "../../components/BookPreview";
 import { Book } from "../../types/Book";
+import { Stack } from "@mui/system";
 
 export default function SearchResult() {
     const location = useLocation();
@@ -10,19 +12,26 @@ export default function SearchResult() {
     return (
         <div>
             <Typography variant="h4" gutterBottom>Søkeresultat: </Typography>
-            
-            {books && books.map((book: Book) => {
-            return (
-                <BookPreview
-                key={book.isbn_13}
-                title={book.title}
-                imageSource={book.coverURL}
-                author={book.author.name}
-                ISBN={book.isbn_13}
-                description={book.description}
-                />
-            );
-            })}
+            <Stack
+                padding="16px"
+                justifyContent="center"
+                direction="row"
+                flexWrap="wrap"
+                gap={2}
+            >
+                {books && books.map((book: Book) => {
+                return (
+                    <BookPreview
+                    key={book.isbn_13}
+                    title={book.title}
+                    imageSource={book.coverURL}
+                    author={book.author.name}
+                    ISBN={book.isbn_13}
+                    description={book.description}
+                    />
+                );
+                })}
+            </Stack>
         </div>
     )
 }
