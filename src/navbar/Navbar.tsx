d1aa6cb483454a0e12a1a0d53ea1d1172840a8e4
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-03-16 08:58:09 +0100

    [#22] feat: Added darkmode compatability

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -1,29 +1,35 @@
-import { useState, useEffect } from 'react';
-import { auth } from '../firebase';
-import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { Link, useNavigate } from 'react-router-dom';
-import logo from "../assets/Logo_ibdb.png"
-import { Button } from '@mui/material';
+import { useState, useEffect } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/Logo_ibdb.png";
+import { Button, Switch } from "@mui/material";
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import { Stack } from "@mui/system";
-import Search from "./components/Search"
+import Search from "./components/Search";
 
-function Navbar() {
+function Navbar(props) {
+  const { theme, settheme } = props;
+
   const [user, setUser] = useState(null);
-  const [admin, setAdmin] = useState(false)
-  const navigate = useNavigate()
+  const [admin, setAdmin] = useState(false);
+  const navigate = useNavigate();
 
   function logOut(auth) {
     signOut(auth);
     setAdmin(false);
-    navigate("/login")
+    navigate("/login");
+  }
+  const handleChange = (event) => {
+    settheme(event.target.checked);
   };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (auth.currentUser?.uid == "kdoILYYTDuWy1izQOTauJxP2rDw1") { //check if its the admins user id
-        setAdmin(true)
+      if (auth.currentUser?.uid == "kdoILYYTDuWy1izQOTauJxP2rDw1") {
+        //check if its the admins user id
+        setAdmin(true);
       }
       setUser(user);
     });
@@ -33,12 +39,13 @@ function Navbar() {
   return (
     <AppBar
       sx={{
-        color: "#FFFFFF",
-        backgroundColor: "#FFFFFF",
+        color: "palette.background.default",
+        backgroundColor: "palette.background.default",
       }}
       position="sticky"
     >
       <Stack
+        bgcolor="palette.background.default"
         marginLeft="10px"
         alignItems="center"
         justifyContent="space-between"
@@ -54,25 +61,30 @@ function Navbar() {
         >
           <img src={logo} alt="error under lasting av logo" width="100px" />
         </Link>
+        <Switch checked={theme} color="success" onChange={handleChange} />
         {user ? (
-          <Button variant="outlined" onClick={() => logOut(auth)}>Sign out</Button>
+          <Button variant="outlined" onClick={() => logOut(auth)}>
+            Sign out
+          </Button>
         ) : (
           <>
-            <Button variant="outlined" onClick={() => navigate('/login')}>Sign in</Button>
-            <Button variant="outlined" onClick={() => navigate('/signup')}>Sign up</Button>
+            <Button variant="outlined" onClick={() => navigate("/login")}>
+              Sign in
+            </Button>
+            <Button variant="outlined" onClick={() => navigate("/signup")}>
+              Sign up
+            </Button>
           </>
         )}
         {admin ? (
-          <Button variant="outlined" onClick={() => navigate('/admin')}>Admin page</Button>
-        ) : 
-          null
-        }
-      
-        
+          <Button variant="outlined" onClick={() => navigate("/admin")}>
+            Admin page
+          </Button>
+        ) : null}
+
         <Search />
       </Stack>
     </AppBar>
   );
 }
 export default Navbar;
-
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,31 @@
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, createTheme, responsiveFontSizes, ThemeProvider, useMediaQuery } from "@mui/material";
 import Navbar from "../navbar/Navbar";
 
 export default function Root() {
+  const [theme, settheme] = useState(false);
+  const darkTheme = createTheme({
+    palette: {
+      mode: theme ? "dark" : "light",
+      primary: {
+        main: theme ? "#3e3e42" : "#FFFFFF",
+      },
+      text: {
+        primary: theme ? "#FFFFFF" : "#3e3e42",
+      },
+    },
+  });
+  // const handleChange = (event) => {
+  //   settheme(event.target.checked);
+  // };
+
   return (
-    <Box width="100%" top={0} margin={0} height="100%">
-      <Navbar />
-      <Outlet />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={theme ? "#3e3e42" : "#fffff"} width="100%" top={0} margin={0} height="100%">
+        <Navbar theme={theme} settheme={settheme} />
+        <Outlet />
+      </Box>
+    </ThemeProvider>
   );
 }
