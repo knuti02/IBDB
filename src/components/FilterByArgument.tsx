424897a5af8f6671cfe0e05f0469066efd30a800
Knut Einar Gilja Johansen <kegjohan@stud.ntnu.no>
2023-03-20 10:34:17 +0100

    [#17] feat: implemented basic filter

--- src/components/FilterByArgument.tsx
+++ src/components/FilterByArgument.tsx
@@ -1,64 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { auth } from '../firebase';
-import useFetch from '../hooks/useFetch';
+  import React, { useState, useEffect } from 'react';
+  import { auth } from '../firebase';
+  import useFetch from '../hooks/useFetch';
 
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import styled from '@emotion/styled';
-import { useNavigate } from 'react-router-dom';
-import { Box } from '@mui/material';
+  import TextField from '@mui/material/TextField';
+  import Button from '@mui/material/Button';
+  import Stack from '@mui/material/Stack';
+  import { Box } from '@mui/material';
 
+  import { Book } from "../types/Book";
+  import BookPreview from "./BookPreview";
 
-const FilterPage = () => {
-  const [filterArgument, setFilterArgument] = useState('genre'); 
-  const [selectedGenre, setSelectedGenre] = useState('');
-  const [books, setBooks] = useState([]);
-  const [searched, setSearched] = useState(false);
+  const FilterPage = () => {
+    const [filterArgument, setFilterArgument] = useState('genre'); 
+    const [selectedGenre, setSelectedGenre] = useState('');
+    const [searched, setSearched] = useState(false);
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const result = await useFetch().getBooks();
-      setBooks(result);
+    const { getBooks, books } = useFetch();
+    const [filteredBooks, setFilteredBooks] = useState([]);
+
+    useEffect(() => {
+      const getInitialData = async () => {
+        await getBooks();
+      };
+      getInitialData();
+    }, []);
+
+    const filterBooksByGenre = () => {
+      if (books) {
+        console.log(books)
+        const filteredBooks = books.filter(book => book.subjects.includes(selectedGenre)).map((book) => ({ data: book }));
+        setFilteredBooks(filteredBooks);
+        console.log(filteredBooks)
+        setSearched(true);
+      }
     };
-    fetchBooks();
-  }, []);
+      
+    return (
+      <Box sx={{ width: '50%', margin: '0 auto' }}>
+      
+        <TextField 
+          id="outlined-basic" 
+          label="Genre" 
+          variant="outlined" 
+          onChange={(e) => setSelectedGenre(e.currentTarget.value)}
+        />
 
-  const filterBooksByGenre = () => {
-    if (books) {
-      const filteredBooks = books.filter(book => book.subjects.includes(selectedGenre));
-      setBooks(filteredBooks);
-      setSearched(true);
-    }
+        <Button variant="contained" onClick={filterBooksByGenre}>
+          Filter
+        </Button>
+
+        <Stack spacing={2}>
+          {books && searched && filteredBooks.length === 0 && <div>No books found</div>}
+          {books && filteredBooks.map((book) => (  
+            <BookPreview
+              key={book.ISBN}
+              title={book.title}
+              imageSource={book.coverURL}
+              author={book.author.name}
+              ISBN={book.ISBN}
+              description={book.description}
+            />
+          ))}
+        </Stack>
+      </Box>
+    );
   };
 
-  const selectGenre = (genre) => {
-    setSelectedGenre(genre);
-  };
-
-  if (!books) {
-    return <div>Loading...</div>;
-  }
-    
-  return (
-    <Box sx={{ width: '50%', margin: '0 auto' }}>
-      <TextField 
-        id="outlined-basic" 
-        label="Genre" 
-        variant="outlined" 
-        onChange={(e) => setSelectedGenre(e.target.value)}
-      />
-      <Button variant="contained" onClick={() => filterBooksByGenre(selectedGenre)}>
-        Filter
-      </Button>
-      <Stack spacing={2}>
-        {searched && books.length === 0 && <div>No books found</div>}
-        {books.map(book => (
-          <div key={book.ISBN}>{book.title}</div>
-        ))}
-      </Stack>
-    </Box>
-  );
-};
-
-export default FilterPage;
+  export default FilterPage;
